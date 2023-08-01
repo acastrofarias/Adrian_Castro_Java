40bf4e681c44283e7f9848c1c788fb4841c5e6ae
Adrian Castro <acastrofarias13@gmail.com>
2023-08-01 16:32:37 -0700

    repo and controller

---
+++ M6-Challenge-Castro-Adrian/customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -0,0 +1,59 @@
+package com.company.customerdataservice.controller;
+
+import com.company.customerdataservice.model.Customer;
+import com.company.customerdataservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+    @Autowired
+    CustomerRepository repo;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return repo.findAll();
+    }
+
+    // create a new customer record
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return repo.save(customer);
+    }
+
+    // update an existing customer record
+    @PutMapping("/customers")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@RequestBody Customer customer) {
+        repo.save(customer);
+    }
+
+    // delete an existing customer record
+    @DeleteMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable int id) {
+        repo.deleteById(id);
+    }
+
+    // find a customer record by id
+    @GetMapping("/customers/{id}")
+    public Customer getCustomerById(@PathVariable int id) {
+
+        Optional<Customer> returnVal = repo.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
+    }
+    // find customer records by state
+    @GetMapping("/customers/state/{state}")
+    public List<Customer> getCustomersByState(@PathVariable String state) {
+        return repo.findByState(state);
+    }
+}
