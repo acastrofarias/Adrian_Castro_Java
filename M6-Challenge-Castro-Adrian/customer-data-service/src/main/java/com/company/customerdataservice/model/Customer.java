be5a6b903c4ef8ece0c065f163441922ea1e0d40
Adrian Castro <acastrofarias13@gmail.com>
2023-08-10 18:41:55 -0700

    fixed repo tests and column names in model

--- M6-Challenge-Castro-Adrian/customer-data-service/src/main/java/com/company/customerdataservice/model/Customer.java
+++ M6-Challenge-Castro-Adrian/customer-data-service/src/main/java/com/company/customerdataservice/model/Customer.java
@@ -15,15 +15,20 @@ public class Customer implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String email;
     private String company;
     private String phone;
+    @Column(name = "address_1")
     private String address1;
+    @Column(name = "address_2")
     private String address2;
     private String city;
     private String state;
+    @Column(name = "postal_code")
     private String postalCode;
     private String country;
 
--- M6-Challenge-Castro-Adrian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
+++ M6-Challenge-Castro-Adrian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
@@ -5,27 +5,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 public class CustomerRepositoryTests {
     @Autowired
     CustomerRepository customerRepository;
 
+    private Customer customer;
+
     @BeforeEach
     public void setUp() throws Exception {
         customerRepository.deleteAll();
-    }
 
-    @Test
-    public void shouldAddCustomer() {
-        // Arrange
-        Customer customer = new Customer();
+        customer = new Customer();
         customer.setFirstName("Mickey");
         customer.setLastName("Mouse");
         customer.setEmail("meeskamooska@gmail.com");
@@ -39,29 +37,16 @@ public class CustomerRepositoryTests {
         customer.setCountry("USA");
 
         customer = customerRepository.save(customer);
+    }
 
+    @Test
+    public void shouldAddCustomer() {
         Optional<Customer> customer1 = customerRepository.findById(customer.getId());
-
         assertEquals(customer1.get(), customer);
     }
 
     @Test
     public void shouldUpdateExistingCustomer() {
-        Customer customer = new Customer();
-        customer.setFirstName("Mickey");
-        customer.setLastName("Mouse");
-        customer.setEmail("meeskamooska@gmail.com");
-        customer.setCompany("Disney");
-        customer.setPhone("021-0569");
-        customer.setAddress1("Magic Ave");
-        customer.setAddress2("Disney World");
-        customer.setCity("Orlando");
-        customer.setState("FL");
-        customer.setPostalCode("00000");
-        customer.setCountry("USA");
-
-        customer = customerRepository.save(customer);
-
         customer.setFirstName("Michael");
         customerRepository.save(customer);
 
@@ -71,89 +56,21 @@ public class CustomerRepositoryTests {
 
     @Test
     public void shouldDeleteExistingCustomer() {
-        Customer customer = new Customer();
-        customer.setFirstName("Mickey");
-        customer.setLastName("Mouse");
-        customer.setEmail("meeskamooska@gmail.com");
-        customer.setCompany("Disney");
-        customer.setPhone("021-0569");
-        customer.setAddress1("Magic Ave");
-        customer.setAddress2("Disney World");
-        customer.setCity("Orlando");
-        customer.setState("FL");
-        customer.setPostalCode("00000");
-        customer.setCountry("USA");
-
-        customer = customerRepository.save(customer);
-
-        Optional<Customer> customer1 = customerRepository.findById(customer.getId());
-
-        assertEquals(customer1.get(), customer);
-
         customerRepository.deleteById(customer.getId());
-
-        customer1 = customerRepository.findById(customer.getId());
-
+        Optional<Customer> customer1 = customerRepository.findById(customer.getId());
         assertFalse(customer1.isPresent());
     }
 
     @Test
     public void shouldGetCustomerById() {
-        Customer customer = new Customer();
-        customer.setFirstName("Mickey");
-        customer.setLastName("Mouse");
-        customer.setEmail("meeskamooska@gmail.com");
-        customer.setCompany("Disney");
-        customer.setPhone("021-0569");
-        customer.setAddress1("Magic Ave");
-        customer.setAddress2("Disney World");
-        customer.setCity("Orlando");
-        customer.setState("FL");
-        customer.setPostalCode("00000");
-        customer.setCountry("USA");
-
-        customer = customerRepository.save(customer);
-
-        Customer customer1 = new Customer();
-        customer1.setFirstName("Charles");
-        customer1.setLastName("Cheese");
-        customer1.setEmail("entertainment@gmail.com");
-        customer1.setCompany("Chuck E Cheese");
-        customer1.setPhone("021-0569");
-        customer1.setAddress1("Family Fun Street");
-        customer1.setAddress2("For Everyone");
-        customer1.setCity("San Diego");
-        customer1.setState("CA");
-        customer1.setPostalCode("00000");
-        customer1.setCountry("USA");
-
-        customer1 = customerRepository.save(customer1);
-
-        Optional<Customer> foundCustomer = customerRepository.findById(customer1.getId());
-
-        assertEquals(foundCustomer.get(), customer1);
+        Optional<Customer> customer1 = customerRepository.findById(customer.getId());
+        assertEquals(customer1.get(), customer);
 
     }
 
     @Test
     public void shouldGetCustomerByState() {
-        Customer customer = new Customer();
-        customer.setFirstName("Mickey");
-        customer.setLastName("Mouse");
-        customer.setEmail("meeskamooska@gmail.com");
-        customer.setCompany("Disney");
-        customer.setPhone("021-0569");
-        customer.setAddress1("Magic Ave");
-        customer.setAddress2("Disney World");
-        customer.setCity("Orlando");
-        customer.setState("FL");
-        customer.setPostalCode("00000");
-        customer.setCountry("USA");
-
-        customer = customerRepository.save(customer);
-
-        List<Customer> customer1 = customerRepository.findByState("FL");
-
-        assertEquals(customer1.size(), 1);
+        List<Customer> customer1 = customerRepository.findByState(customer.getState());
+        assertEquals(1, customer1.size());
     }
 }
