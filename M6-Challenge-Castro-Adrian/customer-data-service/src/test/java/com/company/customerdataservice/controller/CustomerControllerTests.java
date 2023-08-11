b7c240ceb46a32410178b5cc080cf7d7edfaba64
Adrian Castro <acastrofarias13@gmail.com>
2023-08-10 17:14:36 -0700

    customer controller tests and non functional repo tests

---
+++ M6-Challenge-Castro-Adrian/customer-data-service/src/test/java/com/company/customerdataservice/controller/CustomerControllerTests.java
@@ -0,0 +1,89 @@
+package com.company.customerdataservice.controller;
+
+import com.company.customerdataservice.model.Customer;
+import com.company.customerdataservice.repository.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class CustomerControllerTests {
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Customer customer;
+    ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        customer = new Customer();
+        customer.setFirstName("Mickey");
+        customer.setLastName("Mouse");
+        customer.setEmail("meeskamooska@gmail.com");
+        customer.setCompany("Disney");
+        customer.setPhone("021-0569");
+        customer.setAddress1("Magic Ave");
+        customer.setAddress2("Disney World");
+        customer.setCity("Orlando");
+        customer.setState("FL");
+        customer.setPostalCode("00000");
+        customer.setCountry("USA");
+    }
+
+    @Test
+    void addCustomer() throws Exception {
+        String input = mapper.writeValueAsString(customer);
+
+        mockMvc.perform(post("/customers")
+                        .content(input)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        mockMvc.perform(get("/customers/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllCustomers() throws Exception {
+        mockMvc.perform(get("/customers"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateCustomer() throws Exception {
+        customer.setFirstName("UPDATED");
+        String inputJson = mapper.writeValueAsString(customer);
+
+        mockMvc.perform(put("/customers")
+                        .content(inputJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteCustomerById() throws Exception {
+        mockMvc.perform(delete("/customers/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+}
--- M6-Challenge-Castro-Adrian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
+++ M6-Challenge-Castro-Adrian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
@@ -30,11 +30,11 @@ public class CustomerRepositoryTests {
         customer.setLastName("Mouse");
         customer.setEmail("meeskamooska@gmail.com");
         customer.setCompany("Disney");
-        customer.setPhone("212-021-0569");
+        customer.setPhone("021-0569");
         customer.setAddress1("Magic Ave");
-        customer.setAddress2("Disneyworld");
+        customer.setAddress2("Disney World");
         customer.setCity("Orlando");
-        customer.setState("Florida");
+        customer.setState("FL");
         customer.setPostalCode("00000");
         customer.setCountry("USA");
 
@@ -47,21 +47,113 @@ public class CustomerRepositoryTests {
 
     @Test
     public void shouldUpdateExistingCustomer() {
+        Customer customer = new Customer();
+        customer.setFirstName("Mickey");
+        customer.setLastName("Mouse");
+        customer.setEmail("meeskamooska@gmail.com");
+        customer.setCompany("Disney");
+        customer.setPhone("021-0569");
+        customer.setAddress1("Magic Ave");
+        customer.setAddress2("Disney World");
+        customer.setCity("Orlando");
+        customer.setState("FL");
+        customer.setPostalCode("00000");
+        customer.setCountry("USA");
 
+        customer = customerRepository.save(customer);
+
+        customer.setFirstName("Michael");
+        customerRepository.save(customer);
+
+        Optional<Customer> customer1 = customerRepository.findById(customer.getId());
+        assertEquals(customer1.get(), customer);
     }
 
     @Test
     public void shouldDeleteExistingCustomer() {
+        Customer customer = new Customer();
+        customer.setFirstName("Mickey");
+        customer.setLastName("Mouse");
+        customer.setEmail("meeskamooska@gmail.com");
+        customer.setCompany("Disney");
+        customer.setPhone("021-0569");
+        customer.setAddress1("Magic Ave");
+        customer.setAddress2("Disney World");
+        customer.setCity("Orlando");
+        customer.setState("FL");
+        customer.setPostalCode("00000");
+        customer.setCountry("USA");
 
+        customer = customerRepository.save(customer);
+
+        Optional<Customer> customer1 = customerRepository.findById(customer.getId());
+
+        assertEquals(customer1.get(), customer);
+
+        customerRepository.deleteById(customer.getId());
+
+        customer1 = customerRepository.findById(customer.getId());
+
+        assertFalse(customer1.isPresent());
     }
 
     @Test
     public void shouldGetCustomerById() {
+        Customer customer = new Customer();
+        customer.setFirstName("Mickey");
+        customer.setLastName("Mouse");
+        customer.setEmail("meeskamooska@gmail.com");
+        customer.setCompany("Disney");
+        customer.setPhone("021-0569");
+        customer.setAddress1("Magic Ave");
+        customer.setAddress2("Disney World");
+        customer.setCity("Orlando");
+        customer.setState("FL");
+        customer.setPostalCode("00000");
+        customer.setCountry("USA");
+
+        customer = customerRepository.save(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Charles");
+        customer1.setLastName("Cheese");
+        customer1.setEmail("entertainment@gmail.com");
+        customer1.setCompany("Chuck E Cheese");
+        customer1.setPhone("021-0569");
+        customer1.setAddress1("Family Fun Street");
+        customer1.setAddress2("For Everyone");
+        customer1.setCity("San Diego");
+        customer1.setState("CA");
+        customer1.setPostalCode("00000");
+        customer1.setCountry("USA");
+
+        customer1 = customerRepository.save(customer1);
+
+        Optional<Customer> foundCustomer = customerRepository.findById(customer1.getId());
+
+        assertEquals(foundCustomer.get(), customer1);
 
     }
 
     @Test
     public void shouldGetCustomerByState() {
+        Customer customer = new Customer();
+        customer.setFirstName("Mickey");
+        customer.setLastName("Mouse");
+        customer.setEmail("meeskamooska@gmail.com");
+        customer.setCompany("Disney");
+        customer.setPhone("021-0569");
+        customer.setAddress1("Magic Ave");
+        customer.setAddress2("Disney World");
+        customer.setCity("Orlando");
+        customer.setState("FL");
+        customer.setPostalCode("00000");
+        customer.setCountry("USA");
 
+        customer = customerRepository.save(customer);
+
+        List<Customer> customer1 = customerRepository.findByState("FL");
+
+        assertEquals(customer1.size(), 1);
     }
 }
